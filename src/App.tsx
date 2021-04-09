a9d0561e988a64b21cf64eb891dced59ffc815ee
Deron <deron.kao@mypenguinsmart.com>
2021-04-10 01:12:39 +0800

    1. Can add row and delete row 2. Can edit K and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  Container, RowContainer, AddButton, K, Index, Text, DeleteButton, Collision,
+} from "./styles"
+
+interface rowProps {
+  index: number,
+  text: string,
+  handleUpdateText: (index: number, text: string) => void
+  // collision: Array<number>,
+  handleDeleteRow: (index: number) => void,
+}
+
+function Row(props: rowProps) {
+  const { index, text, handleUpdateText, handleDeleteRow } = props;
+  return (
+    <RowContainer>
+      <Index>{index}</Index>
+      <Text
+        value={text}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => { handleUpdateText(index, e.currentTarget.value) }}
+      />
+      <DeleteButton
+        onClick={() => { handleDeleteRow(index) }}
+      >Delete row</DeleteButton>
+      <Collision>123</Collision>
+    </RowContainer>
+  )
+}
 
 function App() {
+  const [k, setK] = useState(+"");
+  const [textList, setTextList] = useState([] as Array<string>);
+  const [isChange, setIsChange] = useState(false);
+
+  function handleUpdateText(index: number, text: string) {
+    textList[index] = text;
+    setTextList(textList);
+    setIsChange(!isChange);
+  }
+
+  function handleAddRow() {
+    textList.push("");
+    setTextList(textList);
+    setIsChange(!isChange);
+  }
+
+  function handleDeleteRow(index: number) {
+    textList.splice(index, 1)
+    setTextList(textList);
+    setIsChange(!isChange);
+  }
+
+  function handleChangeK(e: React.ChangeEvent<HTMLInputElement>) {
+    const n = +e.currentTarget.value
+    if (!isNaN(n - 0) && n < Number.MAX_SAFE_INTEGER) {
+      setK(n)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {textList.map((text: string, index: number) => {
+        return Row({ text, index, handleUpdateText, handleDeleteRow })
+      })}
+      <AddButton onClick={handleAddRow}>Add Button</AddButton>
+      <K
+        value={k}
+        placeholder="K"
+        onInput={handleChangeK}
+      />
+    </Container>
   );
 }
 
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,61 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  width: 80%;
+  margin: 0 10%;
+  border:2px #000000 solid;
+  text-align: -webkit-center;
+  background-color: #FFB7DD;
+  padding: 10px;
+  font-size: 20px;
+`
 
+export const RowContainer = styled.div`
+  display: flex;
+  height: 50px;
+  width: 100%;
+  margin-top: 4px;
+`
+
+export const Collision = styled.div`
+  width: calc(40% - 8px);
+  margin: 0 4px;
+  background-color: #87CEFA;
+  line-height: 50px;
+`
+
+export const AddButton = styled.div`
+  width: calc(100% - 8px);
+  background-color: #87CEFA;
+  margin: 4px;
+  line-height: 50px;
+`
+
+export const K = styled.input`
+  width: calc(100% - 16px);
+  height: 50px;
+  background-color: #87CEFA;
+  line-height: 50px;
+  font-size: 20px;
+`
+
+export const Index = styled.div`
+  width: calc(10% - 8px);
+  margin: 0 4px;
+  line-height: 50px;
+  background-color: #87CEFA;
+`
+
+export const Text = styled.input`
+  width: calc(40% - 8px);
+  margin: 0 4px;
+  background-color: #87CEFA;
+  font-size: 20px;
+`
+
+export const DeleteButton = styled.div`
+  width: calc(20% - 8px);
+  background-color: #87CEFA;
+  line-height: 50px;
+  margin: 0 4px;
 `
